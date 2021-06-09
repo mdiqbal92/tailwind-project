98a4606f11a8cb2e68260de8a5111e94148eab9b
Md Ibal Hossain <iqbal.h1992@gmail.com>
2021-06-09 18:15:58 +0600

    tailwind starting

---
+++ src/components/Projects.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import data from '../data'
+
+const Projects = () => {
+  // eslint-disable-next-line
+  const [items, setItems] = useState(data)
+
+  return (
+    <>
+      <section className="py-20 px-5 bg-purple-800 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
+        {items.map((item) => {
+          const { id, path, desc } = item
+          return (
+            <div key={id}>
+              <img src={path} alt={desc} className="p-5 bg-purple-500 rounded" />
+              <small className="block text-center text-white font-semibold text-lg">
+                {desc}
+              </small>
+            </div>
+          )
+        })}
+      </section>
+    </>
+  )
+}
+
+export default Projects
---
+++ src/components/Showcase.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Showcase = () => {
+  return (
+    <>
+      <section className="showcase">
+        <div className="overlay flex flex-col items-center justify-center text-white px-5">
+          <h1 className="text-5xl font-bold text-right mb-10 lg:text-center lg:text-7xl lg:px-56">
+            Hire me to<span className="text-green-500"> Develop </span>
+            your site
+          </h1>
+
+          <div className="ml-auto lg:ml-0">
+            <Link
+              to="/hire"
+              className="mr-5 bg-purple-500 p-2 px-4 rounded-full font-semibold transition-all hover:bg-purple-800"
+            >
+              Get Started
+            </Link>
+            <Link
+              to="/projects"
+              className="mr-5 bg-white text-purple-500 p-2 px-4 rounded-full font-semibold transition-all hover:bg-purple-800 hover:text-white"
+            >
+              Projects
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Showcase
